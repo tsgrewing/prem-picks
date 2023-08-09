52f0da139b8b57711b238c5aab9bde5a74a42703
Tony Grewing <tsgrewing@gmail.com>
2023-08-09 18:09:41 -0400

    updatin redirect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -15,7 +15,7 @@ function Login() {
       // maybe trigger a loading screen
       return;
     }
-    if (user) navigate("/dashboard");
+    if (user) navigate("/matches");
   }, [user, loading]);
 
   return (
